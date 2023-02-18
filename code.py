a569a0cccd26eff21b6aa46d08e423c54c5d4fd0
Kaj Siebert <kaj@k-si.com>
2023-02-18 10:02:52 +0100

    Switch to polling

--- code.py
+++ code.py
@@ -28,6 +28,7 @@ ssid, password = secrets.WIFI_SSID, secrets.WIFI_PASSWORD  # pylint: disable=no-
 print("Connecting to", ssid)
 wifi.radio.connect(ssid, password)
 print("Connected to", ssid)
+wifi.radio.start_dhcp()
 
 print("My IP address is", wifi.radio.ipv4_address)
 
@@ -36,25 +37,44 @@ server = HTTPServer(pool)
 
 controller = Controller(pixel_pin=board.GP21, num_pixels=300, brightness=0.3, auto_write=False)
 
+current_color = RED
+requested_color = BLUE
 
 @server.route("/")
 def base(request: HTTPRequest):
     """
     Serve the default index.html file.
     """
+    global requested_color
     color = request.query_params.get("color")
     if color:
         color = color.replace("%2C", ",")
         color = [int(x) for x in color.split(",", 3)]
-        color_chase(controller, color)
+        requested_color = color
     with HTTPResponse(request, content_type=MIMEType.TYPE_HTML) as response:
         response.send_file("index.html")
 
 
 
 print(f"Listening on http://{wifi.radio.ipv4_address}:80")
-server.serve_forever(str(wifi.radio.ipv4_address))
+# server.serve_forever(str(wifi.radio.ipv4_address))
+server.start(str(wifi.radio.ipv4_address))
 
+while True:
+    try:
+        # Do something useful in this section,
+        # for example read a sensor and capture an average,
+        # or a running total of the last 10 samples
+
+        # Process any waiting requests
+        if current_color != requested_color:
+            color_chase(controller, requested_color)
+            current_color = requested_color
+
+        server.poll()
+    except OSError as error:
+        print(error)
+        continue
 
 # pixels = controller.pixels
 
