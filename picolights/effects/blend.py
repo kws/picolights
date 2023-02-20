2772c4f8b69b029ea90a5275a095359336d3e6d6
Kaj Siebert <kaj@k-si.com>
2023-02-20 21:30:14 +0100

    Remove unsed property from blend effect

--- picolights/effects/blend.py
+++ picolights/effects/blend.py
@@ -5,18 +5,17 @@ from ..controller import Controller
 
 _quart = 256 // 6
 
-def blend(controller: Controller, color, wait=0):
+def blend(controller: Controller, color):
     pixels = controller.pixels
     num_pixels = controller.num_pixels
 
+    # The color wheel repeats infinetly... in the positive
+    # So we add 256 to the color to make sure we get a positive number when we subtract _quart
     color = color + 256
 
     start_col = colorwheel(color - _quart)
     end_col = colorwheel(color + _quart)
     color = colorwheel(color)
-
-    print("start", start_col)
-    print("end", end_col)
 
     for i in range(num_pixels):
         if i < num_pixels // 2:
