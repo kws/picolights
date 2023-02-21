02f7313af0e84e43dc489dc50e16cb70d30f4274
Kaj Siebert <kaj@k-si.com>
2023-02-21 13:22:55 +0100

    Try circuitpython animation library

--- code.py
+++ code.py
@@ -1,26 +1,14 @@
-import time
 import board
-from picolights.controller import Controller
-
-import alarm
-import board
-import time
-import digitalio
-
-import random
-
-from picolights.effects import color_chase, rainbow_cycle, blend
+import neopixel
+from adafruit_led_animation.animation.sparklepulse import SparklePulse
+import adafruit_led_animation.color as color
 
 
-print("Waking up")
+pixel_pin=board.GP21
+num_pixels=300
 
-controller = Controller(pixel_pin=board.GP21, num_pixels=300, brightness=0.3, auto_write=False)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+blink = SparklePulse(pixels, 0, color.BLUE)
 
-blend(controller, random.randint(0, 256))
-
-# Set an alarm for 60 seconds from now.
-time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 10)
-
-print("Sleeping")
-# Deep sleep until the alarm goes off. Then restart the program.
-alarm.exit_and_deep_sleep_until_alarms(time_alarm)
+while True:
+    blink.animate()
